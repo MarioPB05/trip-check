9244ed3839a9402118ba34622471fb796545ed89
David Perez <davidyluni.personal@gmail.com>
2025-12-21 18:54:36 +0100

    ✨ Create TripButton component

---
+++ src/app/shared/components/trip-button/trip-button.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { ButtonType } from '../../../../core/types/colors.types';
+
+@Component({
+  selector: 'app-trip-button',
+  templateUrl: './trip-button.component.html',
+  styleUrls: ['./trip-button.component.scss'],
+  imports: [NgStyle],
+})
+export class TripButtonComponent {
+  @Input() type: ButtonType = 'primary';
+  @Input() class: string = '';
+  @Input() fill: boolean = false;
+  @Input() disabled: boolean = false;
+  @Output() handleClick = new EventEmitter<Event>();
+
+  typeColors: { [key in ButtonType]: string } = {
+    primary: 'var(--ion-color-primary)',
+    secondary: 'var(--ion-color-secondary)',
+    tertiary: 'var(--ion-color-tertiary)',
+    success: 'var(--ion-color-success)',
+    warning: 'var(--ion-color-warning)',
+    danger: 'var(--ion-color-danger)',
+    light: 'var(--ion-color-light)',
+    medium: 'var(--ion-color-medium)',
+    darkGray: 'var(--ion-color-dark-gray)',
+    dark: 'var(--ion-color-dark)',
+  };
+
+  constructor() {}
+}
