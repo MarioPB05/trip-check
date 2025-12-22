a9a7a873ec2a645f1d8eaf4b9b3b6e8eafff9885
David Perez <davidyluni.personal@gmail.com>
2025-12-22 10:50:42 +0100

    ♻️ Add quantity change event emitter and add max value

--- src/app/shared/components/quantity-counter/quantity-counter.component.ts
+++ src/app/shared/components/quantity-counter/quantity-counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TripButtonComponent } from '../trip-button/trip-button.component';
 import { LucideAngularModule, MinusIcon, PlusIcon } from 'lucide-angular';
 
@@ -12,29 +12,41 @@ export class QuantityCounterComponent {
   protected readonly PlusIcon = PlusIcon;
   protected readonly MinusIcon = MinusIcon;
 
+  @Input() min: number = 0;
+  @Output() quantityChange: any = new EventEmitter<number>();
+
   protected quantity: number = 1;
-  protected min: number = 0;
   protected isInvalid: boolean = false;
+  protected max: number = 9999;
 
   increment() {
-    this.quantity++;
-    this.setInvalidity(this.quantity);
+    if (this.quantity < this.max) {
+      this.quantity++;
+      this.quantityChange.emit(this.quantity);
+      this.setInvalidity(this.quantity);
+    }
   }
 
   decrement() {
     if (this.quantity > this.min) {
       this.quantity--;
+      this.quantityChange.emit(this.quantity);
       this.setInvalidity(this.quantity);
     }
   }
 
   setInvalidity(value: number) {
-    this.isInvalid = isNaN(value) || value < this.min;
+    this.isInvalid = isNaN(value) || value < this.min || value > this.max;
   }
 
   onInputChange(event: any) {
     const value = event.target.value;
     const numericValue = Number(value);
     this.setInvalidity(numericValue);
+
+    if (!this.isInvalid) {
+      this.quantity = numericValue;
+      this.quantityChange.emit(numericValue);
+    }
   }
 }
