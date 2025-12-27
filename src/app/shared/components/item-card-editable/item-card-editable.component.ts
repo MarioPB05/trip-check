4c443ecafb757cb311223efa8ae7354046f7276d
Mario <mperdiguerobarrera@safareyes.es>
2025-12-27 20:16:21 +0100

    ✨ Add editable item card component with quantity counter and emoji display

---
+++ src/app/shared/components/item-card-editable/item-card-editable.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Item } from '@core/models/item.model';
+import { QuantityCounterComponent } from '../quantity-counter/quantity-counter.component';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-item-card-editable',
+  templateUrl: './item-card-editable.component.html',
+  styleUrls: ['./item-card-editable.component.scss'],
+  imports: [QuantityCounterComponent, NgOptimizedImage],
+})
+export class ItemCardEditableComponent {
+  @Input() item!: Item;
+  @Input() helpText: string | null = null;
+  @Output() quantityChange: EventEmitter<number> = new EventEmitter();
+}
