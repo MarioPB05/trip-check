823779b5f3905cd008aafa1652db47e3b2a9782c
David Perez <davidyluni.personal@gmail.com>
2025-12-28 19:40:47 +0100

    🚧 Add trip components to home tab

--- src/app/features/home-tab/home-tab.component.ts
+++ src/app/features/home-tab/home-tab.component.ts
@@ -1,11 +1,14 @@
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent } from '@ionic/angular/standalone';
 import { Component } from '@angular/core';
+import { CurrentTripComponent } from '@features/home-tab/components/current-trip/current-trip.component';
+import { FutureTripComponent } from '@features/home-tab/components/future-trip/future-trip.component';
+import { PastTripComponent } from '@features/home-tab/components/past-trip/past-trip.component';
 
 @Component({
   selector: 'app-home-tab',
   templateUrl: './home-tab.component.html',
   styleUrls: ['./home-tab.component.scss'],
-  imports: [IonTitle, IonToolbar, IonHeader, IonContent],
+  imports: [IonContent, CurrentTripComponent, FutureTripComponent, PastTripComponent],
 })
 export class HomeTabComponent {
   constructor() {}
