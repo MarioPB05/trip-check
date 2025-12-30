517d0ba0f721a2a1d5ca023839ddba2b4f47c82e
Mario <mperdiguerobarrera@safareyes.es>
2025-12-30 18:30:26 +0100

    ✨ Create items page with used and not used item sections and styling

--- src/app/features/item-tab/item-tab.component.ts
+++ src/app/features/item-tab/item-tab.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Component, inject, OnInit } from '@angular/core';
+import { IonContent } from '@ionic/angular/standalone';
+import { ItemService } from '@features/item-tab/services/item.service';
+import { Item } from '@core/models/item.model';
+import { ItemCardComponent } from '@shared/components/item-card/item-card.component';
+import { TripButtonComponent } from '@shared/components/trip-button/trip-button.component';
 
 @Component({
   selector: 'app-item-tab',
   templateUrl: './item-tab.component.html',
   styleUrls: ['./item-tab.component.scss'],
-  imports: [IonTitle, IonToolbar, IonHeader, IonContent],
+  imports: [IonContent, ItemCardComponent, TripButtonComponent],
 })
-export class ItemTabComponent {
-  constructor() {}
+export class ItemTabComponent implements OnInit {
+  private readonly itemService = inject(ItemService);
+
+  protected usedItems: Item[] = [];
+  protected notUsedItems: Item[] = [];
+
+  async ngOnInit() {
+    this.usedItems = await this.itemService.getAllItemsUsed();
+    this.notUsedItems = await this.itemService.getAllItemsNotUsed();
+  }
+
+  openItemDetails(item: Item) {
+    // TODO: Implement item details opening logic
+  }
 }
