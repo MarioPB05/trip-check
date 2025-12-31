67c5116c7f2b823b726cc0b787eee6bd8c69615f
david <davidyluni.personal@gmail.com>
2025-12-31 14:06:39 +0100

    ✨ Add LoadingService to manage loading state and messages

---
+++ src/app/core/services/loading.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoadingService {
+  private loadingCounter = 0;
+
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  loading$ = this.loadingSubject.asObservable();
+
+  private messageSubject = new BehaviorSubject<string>('Cargando...');
+  message$ = this.messageSubject.asObservable();
+
+  show(message: string = 'Cargando...'): void {
+    this.loadingCounter++;
+
+    console.log('LoadingService: show called. Counter:', this.loadingCounter);
+
+    this.messageSubject.next(message);
+    this.loadingSubject.next(true);
+  }
+
+  hide(): void {
+    this.loadingCounter = Math.max(this.loadingCounter - 1, 0);
+
+    if (this.loadingCounter === 0) {
+      this.loadingSubject.next(false);
+    }
+  }
+}
