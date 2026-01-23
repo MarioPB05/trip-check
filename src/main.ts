fbc346810cfe4c3a8be8185bec220ea086956920
david <davidyluni.personal@gmail.com>
2026-01-23 17:10:08 +0100

    ✨ Add path aliases for improved organization

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { AppModule } from './app/app.module';
+import { AppModule } from '@app/app.module';
 import { defineCustomElements as jeepSqliteDefineCustomElements } from 'jeep-sqlite/loader';
 import { platformBrowser } from '@angular/platform-browser';
 
