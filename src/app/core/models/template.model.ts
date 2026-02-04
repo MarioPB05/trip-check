4a99b34e662c389d04d80a8817ccc00ef31840e0
david <davidyluni.personal@gmail.com>
2026-02-04 11:30:01 +0100

    ♻️ Update Template interface to use ItemWithQuantity and remove templateItem

--- src/app/core/models/template.model.ts
+++ src/app/core/models/template.model.ts
@@ -1,13 +1,8 @@
-import { Item } from '@core/models/item.model';
+import { ItemWithQuantity } from '@core/models/item.model';
 
 export interface Template {
   id: number;
   name: string;
-  items: TemplateItem[];
+  items: ItemWithQuantity[];
   deleted: boolean;
-}
-
-export interface TemplateItem {
-  item: Item;
-  quantity: number;
 }
