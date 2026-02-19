599ff89a53e19e255c0ce62d85aac1762b9bff6c
david <davidyluni.personal@gmail.com>
2026-02-19 16:41:46 +0100

    ✨ Add CRUD operations for templates and template items in repository

--- src/app/core/repositories/template.repository.ts
+++ src/app/core/repositories/template.repository.ts
@@ -39,6 +39,54 @@ export class TemplateRepository {
     return Object.values(templatesMap);
   }
 
+  async getAllTemplates(): Promise<any[]> {
+    return this.db.withConn(async (conn) => {
+      const res = await conn.query(`
+        SELECT t.id, t.name, t.deleted, i.id as item_id, i.name as item_name, i.emoji as item_emoji, ti.quantity as item_quantity
+        FROM template t
+        LEFT JOIN template_item ti on t.id = ti.template_id
+        LEFT JOIN item i on ti.item_id = i.id and i.deleted = 0
+        WHERE t.deleted = 0
+        ORDER BY t.id
+      `);
+
+      return this.formatDBResultToTemplates(res.values || []);
+    });
+  }
+
+  async getTemplateById(id: number): Promise<Template | null> {
+    return await this.db.withConn(async (conn) => {
+      const res = await conn.query(
+        `
+        SELECT t.id, t.name, t.deleted, i.id as item_id, i.name as item_name, i.emoji as item_emoji, ti.quantity as item_quantity
+        FROM template t
+        LEFT JOIN template_item ti on t.id = ti.template_id
+        LEFT JOIN item i on ti.item_id = i.id and i.deleted = 0
+        WHERE t.deleted = 0 AND t.id = ?
+      `,
+        [id],
+      );
+
+      return this.formatDBResultToTemplates(res.values || [])[0] || null;
+    });
+  }
+
+  async getItemsByTemplateId(templateId: number): Promise<{ itemId: number; quantity: number }[]> {
+    return await this.db.withConn(async (conn) => {
+      const res = await conn.query(
+        'SELECT item_id, quantity FROM template_item WHERE template_id = ?',
+        [templateId],
+      );
+
+      return (
+        res.values?.map((row) => ({
+          itemId: row['item_id'],
+          quantity: row['quantity'],
+        })) || []
+      );
+    });
+  }
+
   async createTemplate(name: string): Promise<number | null> {
     return this.db.withConn(async (conn) => {
       const res = await conn.run('INSERT INTO template (name) VALUES (?)', [name]);
@@ -55,18 +103,33 @@ export class TemplateRepository {
     });
   }
 
-  async getAllTemplates(): Promise<any[]> {
-    return this.db.withConn(async (conn) => {
-      const res = await conn.query(`
-        SELECT t.id, t.name, t.deleted, i.id as item_id, i.name as item_name, i.emoji as item_emoji, ti.quantity as item_quantity
-        FROM template t
-        LEFT JOIN template_item ti on t.id = ti.template_id
-        LEFT JOIN item i on ti.item_id = i.id and i.deleted = 0
-        WHERE t.deleted = 0
-        ORDER BY t.id
-      `);
+  async removeItemFromTemplate(templateId: number, itemId: number): Promise<void> {
+    await this.db.withConn(async (conn) => {
+      await conn.run('DELETE FROM template_item WHERE template_id = ? AND item_id = ?', [
+        templateId,
+        itemId,
+      ]);
+    });
+  }
 
-      return this.formatDBResultToTemplates(res.values || []);
+  async removeAllItemsFromTemplate(templateId: number): Promise<void> {
+    await this.db.withConn(async (conn) => {
+      await conn.run('DELETE FROM template_item WHERE template_id = ?', [templateId]);
+    });
+  }
+
+  async updateTemplateName(templateId: number, name: string) {
+    await this.db.withConn(async (conn) => {
+      await conn.run('UPDATE template SET name = ? WHERE id = ?', [name, templateId]);
+    });
+  }
+
+  updateItemQuantity(templateId: number, itemId: number, quantity: number) {
+    return this.db.withConn(async (conn) => {
+      await conn.run(
+        'UPDATE template_item SET quantity = ? WHERE template_id = ? AND item_id = ?',
+        [quantity, templateId, itemId],
+      );
     });
   }
 }
