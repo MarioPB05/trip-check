3c9f1db9d6102d5b4b0c3d0825fdeb72ee96e239
david <davidyluni.personal@gmail.com>
2026-02-20 09:15:17 +0100

    ✨ Add no items message in location items component

--- src/app/shared/components/location-items/location-items.component.ts
+++ src/app/shared/components/location-items/location-items.component.ts
@@ -3,14 +3,17 @@ import { Location } from '@core/models/location.model';
 import { ItemCardComponent } from '@shared/components/item-card/item-card.component';
 import { ItemWithQuantity } from '@core/models/item.model';
 import { Template } from '@core/models/template.model';
+import { LucideAngularModule, Shirt } from 'lucide-angular';
 
 @Component({
   selector: 'app-location-items',
   templateUrl: './location-items.component.html',
   styleUrls: ['./location-items.component.scss'],
-  imports: [ItemCardComponent],
+  imports: [ItemCardComponent, LucideAngularModule],
 })
 export class LocationItemsComponent {
+  protected readonly shirtIcon = Shirt;
+
   @Input() location!: Location | Template;
   @Input() items: ItemWithQuantity[] = [];
 }
